4733eed6dc6dda06b7685e1ecc82dc74e7369e4e
shahryar200 <toughi_1992@yahoo.com>
2019-12-19 13:43:42 +0330

    Update run.py

--- run.py
+++ run.py
@@ -6,7 +6,7 @@ my_awesome_app = Flask(__name__)
 @my_awesome_app.route('/', defaults={'path': ''})
 @my_awesome_app.route('/<path:path>')
 def catch_all(path):
-    return '5TxlwLDdy1Jqp91_sUScLFmOrVnDaCjXkFJsiXApqZw.Ej1w25iUPyr7XjksoC2O3tjhvulcwN2sE3Aha8IH8lQ'
+    return 'Ej1w25iUPyr7XjksoC2O3tjhvulcwN2sE3Aha8IH8lQ'
 
 
 if __name__ == '__main__':
