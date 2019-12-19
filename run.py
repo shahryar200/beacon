2c159a8d5722f9c9d1cd5d136cc5c44376153808
shahryar200 <toughi_1992@yahoo.com>
2019-12-19 13:09:10 +0330

    Add files via upload

---
+++ run.py
@@ -0,0 +1,13 @@
+from flask import Flask
+
+my_awesome_app = Flask(__name__)
+
+
+@my_awesome_app.route('/', defaults={'path': ''})
+@my_awesome_app.route('/<path:path>')
+def catch_all(path):
+    return 'PyaMw5uTXJNsrKcCMrZOIBEkKZw61QQOXru9DAOTYH4.Ej1w25iUPyr7XjksoC2O3tjhvulcwN2sE3Aha8IH8lQ'
+
+
+if __name__ == '__main__':
+    my_awesome_app.run()
